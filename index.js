404b7124cfa6a108ab008f35751f3c133dfff6dd
Lyman Lai <lyman.lai@yaha.me>
2017-05-23 11:04:37 +0800

    update text

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var server = http.createServer(function (req, res) {
     res.writeHead(200);
-    res.end('heyaa');
+    res.end('new stuff');
   })
   .listen(process.env.PORT || 3000, function () {
     console.log('App listening on port 3000');
