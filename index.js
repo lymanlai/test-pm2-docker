dc30cb47b94ac41cf81ff42a9f0af81e075661e6
Lyman Lai <lyman.lai@yaha.me>
2017-05-23 10:50:53 +0800

    add Dockerfile

---
+++ index.js
@@ -0,0 +1,8 @@
+var http = require('http');
+var server = http.createServer(function (req, res) {
+    res.writeHead(200);
+    res.end('heyaa');
+  })
+  .listen(process.env.PORT || 3000, function () {
+    console.log('App listening on port 3000');
+  });
